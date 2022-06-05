d84d6108e4453df76bea66306f53927f9c484c2b
jrodolforeis <joserodolfomendes@hotmail.com>
2022-06-05 19:04:22 -0300

    smile works

--- objectDetection.py
+++ objectDetection.py
@@ -13,6 +13,13 @@ def detectAndDisplay(frame):
         frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
 
         faceROI = frame_gray[y:y+h,x:x+w]
+
+        roi_gray = frame_gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+        for (sx, sy, sw, sh) in smiles:
+            cv.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+
         #-- In each face, detect eyes
         eyes = eyes_cascade.detectMultiScale(faceROI)
         for (x2,y2,w2,h2) in eyes:
@@ -20,29 +27,22 @@ def detectAndDisplay(frame):
             radius = int(round((w2 + h2)*0.25))
             frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
 
-        gray = cv.cvtColor(frame_gray, cv.COLOR_BGR2GRAY)
-        mouth_rects = smile_cascade.detectMultiScale(gray, 1.7, 11)
-        for (x, y, w, h) in mouth_rects:
-            y = int(y - 0.15 * h)
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            break
+
 
     cv.imshow('Capture - Face detection', frame)
 
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
 parser.add_argument('--face_cascade', help='Path to face cascade.', default='./haarcascade_frontalface_alt.xml')
 parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='./haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--smile_cascade', help='Path to smile cascade.', default='./haarcascade_smile.xml')
 parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
 args = parser.parse_args()
 
 face_cascade_name = args.face_cascade
 eyes_cascade_name = args.eyes_cascade
-smile_cascade_name = args.smile_cascade
 
 face_cascade = cv.CascadeClassifier()
 eyes_cascade = cv.CascadeClassifier()
-smile_cascade = cv.CascadeClassifier()
+smile_cascade = cv.CascadeClassifier('haarcascade_smile.xml')
 
 #-- 1. Load the cascades
 if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
@@ -50,9 +50,6 @@ if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
     exit(0)
 if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
     print('--(!)Error loading eyes cascade')
-    exit(0)
-if not smile_cascade.load(cv.samples.findFile(smile_cascade_name)):
-    print('--(!)Error loading smile cascade')
     exit(0)
 
 camera_device = args.camera
